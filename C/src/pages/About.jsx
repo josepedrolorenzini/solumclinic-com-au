b05d3e8a7ffec5714a58e66f6e23e39a9a4aa195
josepedrolorenzini <webmaster@marbleceramiccorp.com.au>
2025-11-13 23:22:54 +1100

    bio in about.jsx

--- C/src/pages/About.jsx
+++ C/src/pages/About.jsx
@@ -7,8 +7,16 @@ const About = ({title}) =>{
  return  (
     <>
     <h2>{title}</h2>
-    <p>We are a team of developers who are passionate about creating web applications.</p>
-    <p>just a web developer fetch customer from supabase</p>
+    <p
+    className='font-light text-cyan-400'
+    >Hello, I'm Joseph Lorenzini, a digital nomad passionate about the internet, web development technology, 
+    and cryptocurrency. I founded Viento del Sur in 2009 to facilitate internet accessibility to everyone.
+    <br /><br />
+    I have maintained a commitment to open source and freedom since the beginning of this project. 
+    My intention is to continue delivering digital solutions with humility and accessibility for everyone.
+    <br /><br />
+    I studied Graphic Design in Chile and earned my degree in Computer Science in Australia in 2023. 
+    As a freelance developer, I'm always ready to solve your challenges wherever I am, as long as there's an internet connection.</p>
  
     </>
     
